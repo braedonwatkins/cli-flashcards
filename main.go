672a678e348bc101c84676a22463a795fedb2254
braedonwatkins <watkins.braedon@gmail.com>
2025-11-15 12:11:44 -0500

    add lipgloss to style cards w/ dialog pattern

--- main.go
+++ main.go
@@ -2,10 +2,60 @@ package main
 
 import (
 	"fmt"
+	"image/color"
 	"os"
+	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
+	lipgloss "github.com/charmbracelet/lipgloss"
+	"github.com/lucasb-eyer/go-colorful"
+	"github.com/muesli/gamut"
 )
+
+var (
+	dialogBoxStyle = lipgloss.NewStyle().
+			Border(lipgloss.RoundedBorder()).
+			BorderForeground(lipgloss.Color("#874BFD")).
+			Padding(1, 0).
+			BorderTop(true).
+			BorderLeft(true).
+			BorderRight(true).
+			BorderBottom(true)
+
+	buttonStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("#FFF7DB")).
+			Background(lipgloss.Color("#888B7E")).
+			Padding(0, 3).
+			MarginTop(1)
+
+	activeButtonStyle = buttonStyle.
+				Foreground(lipgloss.Color("#FFF7DB")).
+				Background(lipgloss.Color("#F25D94")).
+				MarginRight(2).
+				Underline(true)
+
+	subtle = lipgloss.AdaptiveColor{Light: "#D9DCCF", Dark: "#383838"}
+	blends = gamut.Blends(lipgloss.Color("#F25D94"), lipgloss.Color("#EDFF82"), 50)
+)
+
+const (
+	// In real life situations we'd adjust the document to fit the width we've
+	// detected. In the case of this example we're hardcoding the width, and
+	// later using the detected width only to truncate in order to avoid jaggy
+	// wrapping.
+	width = 96
+
+	columnWidth = 30
+)
+
+func rainbow(base lipgloss.Style, s string, colors []color.Color) string {
+	var str string
+	for i, ss := range s {
+		color, _ := colorful.MakeColor(colors[i%len(colors)])
+		str = str + base.Foreground(lipgloss.Color(color.Hex())).Render(string(ss))
+	}
+	return str
+}
 
 type model struct {
 	tea.Model // FIXME: why dont the docs mention this?
@@ -47,17 +97,30 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() string {
-	s := "This is the header\n\n"
+	s := strings.Builder{}
 
-	s += fmt.Sprintf("%s\n", testCards[m.CardIndex].Front)
+	// Dialog Scope
+	{
+		question := lipgloss.NewStyle().Width(50).Align(lipgloss.Center).Render(rainbow(lipgloss.NewStyle(), testCards[m.CardIndex].Front, blends))
 
-	if m.Revealed == true {
-		s += fmt.Sprintf("%s\n", testCards[m.CardIndex].Back)
+		answer := ""
+		if m.Revealed == true {
+			answer = lipgloss.NewStyle().Width(50).Align(lipgloss.Center).Render(testCards[m.CardIndex].Back)
+		}
+
+		ui := lipgloss.JoinVertical(lipgloss.Center, question, answer)
+
+		dialog := lipgloss.Place(width, 9,
+			lipgloss.Center, lipgloss.Center,
+			dialogBoxStyle.Render(ui),
+			lipgloss.WithWhitespaceChars("猫咪"),
+			lipgloss.WithWhitespaceForeground(subtle),
+		)
+
+		s.WriteString(dialog + "\n\n")
 	}
 
-	s += "\nPress q to quit.\n"
-
-	return s
+	return s.String()
 }
 
 func main() {
--- mock.go
+++ mock.go
@@ -8,4 +8,5 @@ type flashCard struct {
 var testCards = []flashCard{
 	{Front: "What is the first letter of the alphabet", Back: "A"},
 	{Front: "What is the last letter of the alphabet", Back: "Z"},
+	{Front: "WWhat is the last letter of the alphabetWhat is the last letter of the alphabetWhat is the last letter of the alphabetWhat is the last letter of the alphabetWhat is the last letter of the alphabetWhat is the last letter of the alphabetWhat is the last letter of the alphabetWhat is the last letter of the alphabetWhat is the last letter of the alphabetWhat is the last letter of the alphabetWhat is the last letter of the alphabetWhat is the last letter of the alphabetWhat is the last letter of the alphabetWhat is the last letter of the alphabetWhat is the last letter of the alphabetWhat is the last letter of the alphabetWhat is the last letter of the alphabethat is the last letter of the alphabet", Back: "Z"},
 }

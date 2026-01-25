b635d607210b1538b96f357fc01532d4b4e0bead
braedonwatkins <watkins.braedon@gmail.com>
2026-01-24 21:25:36 -0500

    re-routing main for parser for now

--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
 
-import (
-	"fmt"
-	"os"
-
-	tea "github.com/charmbracelet/bubbletea"
-)
+// import (
+// 	"fmt"
+// 	"os"
+//
+// 	tea "github.com/charmbracelet/bubbletea"
+// )
 
 func main() {
-	p := tea.NewProgram(initialModel())
-	if _, err := p.Run(); err != nil {
-		fmt.Printf("Error: %v", err)
-		os.Exit(1)
-	}
+	parse()
+	// p := tea.NewProgram(initialModel())
+	// if _, err := p.Run(); err != nil {
+	// 	fmt.Printf("Error: %v", err)
+	// 	os.Exit(1)
+	// }
 }
